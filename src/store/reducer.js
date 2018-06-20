5b117f0e03a0d255fea4ddd08f26d0718968234a
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-06-20 11:53:07 -0400

    Pretty up the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,29 +3,28 @@ const intialState = {
 }
 
 const reducer = (state = intialState, action) =>{
-    if(action.type === 'INCREMENT'){
-        return{
-            counter: state.counter + 1
-        }
-    }
-    if(action.type === 'DECREMENT'){
-        return{
-            counter: state.counter - 1
-        }
-    }
-    if(action.type === 'ADD'){
-        return{
-            counter: state.counter + 5
-        }
-    }
-    if(action.type === 'SUBTRACT'){
-        return{
-            counter: state.counter - 5
-        }
-    }
-     
 
-    return state;
+    switch(action.type){
+        case 'INCREMENT':
+            return{
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return{
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return{
+                counter: state.counter + action.payload.value
+            }
+        case 'SUBTRACT':
+            return{
+                counter: state.counter - action.payload.value
+            }
+         default:
+            return state
+    }
+    
 };
 
 export default reducer;
